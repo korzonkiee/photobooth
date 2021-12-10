71642ec62d4cad4d617bfdb4abf35691e237fbe1
Jeroen Meijer (Jay) <jeroenfkmeijer@gmail.com>
2021-12-10 16:04:30 +0100

    feat(session_repository): add creating session and add picture calls (#33)

--- lib/landing/widgets/landing_body.dart
+++ lib/landing/widgets/landing_body.dart
@@ -122,7 +122,7 @@ class _SessionDebugCard extends StatelessWidget {
                           ],
                         );
                       },
-                    )
+                    ),
                   ],
                 );
               },
