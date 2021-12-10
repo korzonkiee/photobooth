ea32ad4fdfb5bc739ef739fce8a2c28345ae73e3
Erick Zanardo <erickzanardoo@gmail.com>
2021-12-10 12:29:33 -0300

    feat: setup page now creates a session

--- lib/session_setup/cubit/session_setup_state.dart
+++ lib/session_setup/cubit/session_setup_state.dart
@@ -1,33 +1,52 @@
 part of 'session_setup_cubit.dart';
 
+enum SessionSetupStatus {
+  initial,
+  loading,
+  success,
+  failure,
+}
+
 class SessionSetupState extends Equatable {
   const SessionSetupState({
+    required this.createdSessionId,
     required this.selectedPrompt,
     required this.promptList,
+    required this.status,
   });
 
   const SessionSetupState.initial()
       : this(
+          createdSessionId: '',
           selectedPrompt: '',
           promptList: prompts,
+          status: SessionSetupStatus.initial,
         );
 
+  final SessionSetupStatus status;
   final String selectedPrompt;
   final List<String> promptList;
+  final String createdSessionId;
 
   @override
   List<Object?> get props => [
         selectedPrompt,
         promptList,
+        status,
+        createdSessionId,
       ];
 
   SessionSetupState copyWith({
+    String? createdSessionId,
     String? selectedPrompt,
     List<String>? promptList,
+    SessionSetupStatus? status,
   }) {
     return SessionSetupState(
+      createdSessionId: createdSessionId ?? this.createdSessionId,
       selectedPrompt: selectedPrompt ?? this.selectedPrompt,
       promptList: promptList ?? this.promptList,
+      status: status ?? this.status,
     );
   }
 }
--- lib/session_setup/view/session_setup_page.dart
+++ lib/session_setup/view/session_setup_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_photobooth/session_setup/session_setup.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:session_repository/session_repository.dart';
 
 class SessionSetupPage extends StatelessWidget {
   const SessionSetupPage({Key? key}) : super(key: key);
@@ -13,7 +14,10 @@ class SessionSetupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => SessionSetupCubit(),
+      create: (context) {
+        final sessionRepository = context.read<SessionRepository>();
+        return SessionSetupCubit(sessionRepository: sessionRepository);
+      },
       child: const SessionSetupView(),
     );
   }
@@ -24,62 +28,79 @@ class SessionSetupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cubit = context.watch<SessionSetupCubit>();
-    final state = cubit.state;
-
-    return Scaffold(
-      floatingActionButton: state.selectedPrompt.isNotEmpty
-          ? FloatingActionButton(
-              onPressed: () {
-                //
-              },
-              child: const Icon(Icons.forward),
-            )
-          : null,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              'Choose a prompt bellow:',
-              style: Theme.of(context).textTheme.headline3,
+    return BlocConsumer<SessionSetupCubit, SessionSetupState>(
+      listener: (context, state) {
+        if (state.status == SessionSetupStatus.failure) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Errors, sorry, try again'),
             ),
-            TextField(
-              onChanged: cubit.changedQuery,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Wrap(
-                  children: [
-                    for (final prompt in state.promptList)
-                      InkWell(
-                        onTap: () {
-                          cubit.selectPrompt(prompt);
-                        },
-                        child: Container(
-                          width: 240,
-                          height: 120,
-                          padding: const EdgeInsets.all(8),
-                          child: Card(
-                            color: prompt == state.selectedPrompt
-                                ? Theme.of(context).primaryColor
-                                : null,
-                            child: Center(
-                              child: Text(
-                                prompt,
-                                textAlign: TextAlign.center,
+          );
+        } else if (state.status == SessionSetupStatus.success) {
+          print(state.createdSessionId);
+          // TODO Navigate
+        }
+      },
+      builder: (context, state) {
+        final continueButtonEnabled = state.selectedPrompt.isNotEmpty &&
+            state.status != SessionSetupStatus.loading;
+        return Scaffold(
+          floatingActionButton: continueButtonEnabled
+              ? FloatingActionButton(
+                  onPressed: () {
+                    context.read<SessionSetupCubit>().createSession();
+                  },
+                  child: const Icon(Icons.forward),
+                )
+              : null,
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  'Choose a prompt bellow:',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                TextField(
+                  onChanged: context.read<SessionSetupCubit>().changedQuery,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Wrap(
+                      children: [
+                        for (final prompt in state.promptList)
+                          InkWell(
+                            onTap: () {
+                              context
+                                  .read<SessionSetupCubit>()
+                                  .selectPrompt(prompt);
+                            },
+                            child: Container(
+                              width: 240,
+                              height: 120,
+                              padding: const EdgeInsets.all(8),
+                              child: Card(
+                                color: prompt == state.selectedPrompt
+                                    ? Theme.of(context).primaryColor
+                                    : null,
+                                child: Center(
+                                  child: Text(
+                                    prompt,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                  ],
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
