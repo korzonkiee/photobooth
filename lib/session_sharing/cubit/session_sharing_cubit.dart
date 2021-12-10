8c3a09236908b585d178c91e0bad1addf0dc7b32
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2021-12-10 17:21:14 +0100

    feat: add initial session sharing page

--- lib/session_sharing/cubit/session_sharing_cubit.dart
+++ lib/session_sharing/cubit/session_sharing_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:session_repository/session_repository.dart';
@@ -6,8 +8,50 @@ part 'session_sharing_state.dart';
 
 class SessionSharingCubit extends Cubit<SessionSharingState> {
   SessionSharingCubit({
-    // TODO(alejandro): USE sessionRepository
-    // ignore: avoid_unused_constructor_parameters
     required SessionRepository sessionRepository,
-  }) : super(const SessionSharingState.initial());
+    required String sessionId,
+  })  : _sessionRepository = sessionRepository,
+        super(SessionSharingState(
+          sessionId: sessionId,
+        ));
+
+  final SessionRepository _sessionRepository;
+
+  StreamSubscription<Session>? _sessionSubscription;
+
+  Future<void> subscribe() async {
+    await _sessionSubscription?.cancel();
+    emit(state.copyWith(status: SessionSharingStatus.loading));
+    try {
+      _sessionSubscription = _sessionRepository
+          .getSession(state.sessionId)
+          .listen(_onSessionUpdated);
+    } catch (e) {
+      emit(state.copyWith(status: SessionSharingStatus.failure));
+    }
+  }
+
+  Future<void> _onSessionUpdated(Session session) async {
+    emit(state.copyWith(
+      session: session,
+    ));
+
+    if (session.isInvalid) {
+      emit(state.copyWith(status: SessionSharingStatus.failure));
+      return;
+    }
+
+    final downloadablePhotoUrls =
+        await _sessionRepository.getDownloadUrls(session.photoUrls);
+    emit(state.copyWith(
+      downloadablePhotoUrls: downloadablePhotoUrls,
+      status: SessionSharingStatus.success,
+    ));
+  }
+
+  @override
+  Future<void> close() async {
+    await _sessionSubscription?.cancel();
+    await super.close();
+  }
 }
--- lib/session_sharing/view/session_sharing_page.dart
+++ lib/session_sharing/view/session_sharing_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:io_photobooth/session_sharing/session_sharing.dart';
 import 'package:session_repository/session_repository.dart';
 
@@ -16,7 +18,8 @@ class SessionSharingPage extends StatelessWidget {
     return BlocProvider(
       create: (_) => SessionSharingCubit(
         sessionRepository: context.read<SessionRepository>(),
-      ),
+        sessionId: sessionId,
+      )..subscribe(),
       child: const SessionSharingView(),
     );
   }
@@ -27,9 +30,105 @@ class SessionSharingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Share your session!'),
+    final textTheme = Theme.of(context).textTheme;
+
+    final state = context.watch<SessionSharingCubit>().state;
+
+    return BlocListener<SessionSharingCubit, SessionSharingState>(
+      listener: (context, state) {
+        if (state.status == SessionSharingStatus.failure) {
+          context.go('/404');
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 48),
+              Text(
+                'Sharing Page for Session: ${state.sessionId}',
+                style: textTheme.headline2,
+              ),
+              const SizedBox(height: 8),
+              if (state.status == SessionSharingStatus.failure)
+                const Text(
+                  'Error loading session. '
+                  'Are you sure the ID is correct?',
+                )
+              else if (state.session == null)
+                const _LoadingText(
+                  child: Text('Loading session...'),
+                )
+              else ...[
+                const _PromptHeader(),
+                if (state.downloadablePhotoUrls == null)
+                  const _LoadingText(
+                    child: Text('Downloading photos...'),
+                  )
+                else if (state.downloadablePhotoUrls!.isEmpty)
+                  const Text('No photos have been uploaded yet.')
+                else
+                  Column(
+                    children: [
+                      for (final url in state.downloadablePhotoUrls!) ...[
+                        Material(
+                          elevation: 2,
+                          borderRadius: BorderRadius.circular(8),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: Image.network(url),
+                        ),
+                        const SizedBox(height: 4),
+                      ],
+                    ],
+                  ),
+              ]
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoadingText extends StatelessWidget {
+  const _LoadingText({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        child,
+        const SizedBox(width: 8),
+        const CupertinoActivityIndicator(),
+      ],
+    );
+  }
+}
+
+class _PromptHeader extends StatelessWidget {
+  const _PromptHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    final prompt = context
+        .select((SessionSharingCubit cubit) => cubit.state.session!.prompt);
+
+    return Card(
+      margin: const EdgeInsets.all(16),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(
+          prompt,
+          style: textTheme.headline2,
+        ),
       ),
     );
   }
