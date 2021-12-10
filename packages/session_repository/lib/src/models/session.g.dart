28a474fe58cfc3e1dcc3042300a5c43b99d1e745
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2021-12-10 14:16:50 +0100

    fix json naming convention

--- packages/session_repository/lib/src/models/session.g.dart
+++ packages/session_repository/lib/src/models/session.g.dart
@@ -1,23 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: implicit_dynamic_parameter
+
 part of 'session.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Session _$SessionFromJson(Map<String, dynamic> json) => Session(
+Session _$SessionFromJson(Map json) => Session(
       id: json['id'] as String,
       createdAt:
-          const TimestampConverter().fromJson(json['createdAt'] as Timestamp),
+          const TimestampConverter().fromJson(json['created_at'] as Timestamp),
       prompt: json['prompt'] as String,
-      photoUrls:
-          (json['photoUrls'] as List<dynamic>).map((e) => e as String).toList(),
+      photoUrls: (json['photo_urls'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
     );
 
 Map<String, dynamic> _$SessionToJson(Session instance) => <String, dynamic>{
       'id': instance.id,
-      'createdAt': const TimestampConverter().toJson(instance.createdAt),
+      'created_at': const TimestampConverter().toJson(instance.createdAt),
       'prompt': instance.prompt,
-      'photoUrls': instance.photoUrls,
+      'photo_urls': instance.photoUrls,
     };
