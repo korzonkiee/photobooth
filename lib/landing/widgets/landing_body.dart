c33ce995f3bc02121d428ed4cbeea69f481b6dfe
Jeroen Meijer (Jay) <jeroenfkmeijer@gmail.com>
2021-12-10 15:37:33 +0100

    feat(session_repository): cache sessions and add download url method (#28)

--- lib/landing/widgets/landing_body.dart
+++ lib/landing/widgets/landing_body.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/landing/landing.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:session_repository/session_repository.dart';
 
 class LandingBody extends StatelessWidget {
   const LandingBody({Key? key}) : super(key: key);
@@ -15,6 +18,10 @@ class LandingBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
+          const SizedBox(height: 48),
+          const Center(
+            child: _SessionDebugCard(),
+          ),
           const SizedBox(height: 48),
           SelectableText(
             l10n.landingPageHeading,
@@ -44,6 +51,84 @@ class LandingBody extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _SessionDebugCard extends StatelessWidget {
+  const _SessionDebugCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final repo = context.read<SessionRepository>();
+
+    return Card(
+      margin: const EdgeInsets.all(16),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('DEBUG SESSION /sessions/example'),
+            const SizedBox(height: 8),
+            StreamBuilder<Session>(
+              stream: repo.getSession('example'),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text(
+                    'Error\n${snapshot.error}\n${snapshot.stackTrace}',
+                  );
+                } else if (!snapshot.hasData) {
+                  return const CupertinoActivityIndicator();
+                }
+
+                final session = snapshot.data!;
+
+                return Column(
+                  children: [
+                    Text('$session'),
+                    const SizedBox(height: 8),
+                    Text('Contains ${session.photoUrls.length} photos.'),
+                    const SizedBox(height: 4),
+                    FutureBuilder<List<String>>(
+                      future: repo.getDownloadUrls(session.photoUrls),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text(
+                            'Error\n${snapshot.error}\n${snapshot.stackTrace}',
+                          );
+                        } else if (!snapshot.hasData) {
+                          return Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              CupertinoActivityIndicator(),
+                              SizedBox(width: 4),
+                              Text('Downloading URLs...'),
+                            ],
+                          );
+                        }
+
+                        final urls = snapshot.data!;
+
+                        return Column(
+                          children: [
+                            for (final url in urls)
+                              Image.network(
+                                url,
+                                width: 300,
+                              ),
+                          ],
+                        );
+                      },
+                    )
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
