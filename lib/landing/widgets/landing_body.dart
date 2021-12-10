c51ea784783d282d97687ca93750b40730fd7bc0
Erick Zanardo <erickzanardoo@gmail.com>
2021-12-10 14:23:32 -0300

    fix: addressing some lint issues

--- lib/landing/widgets/landing_body.dart
+++ lib/landing/widgets/landing_body.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/landing/landing.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
-import 'package:session_repository/session_repository.dart';
 
 class LandingBody extends StatelessWidget {
   const LandingBody({Key? key}) : super(key: key);
