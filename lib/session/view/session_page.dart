bfca98a3918f90d784c849003b939d3a5e6932f0
Maciej Korzeniewski <mck.korzeniewski@gmail.com>
2021-12-10 13:20:21 +0100

    feat: session page scaffold

---
+++ lib/session/view/session_page.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:io_photobooth/session/session.dart';
+import 'package:io_photobooth/stickers/stickers.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:very_good_analysis/very_good_analysis.dart';
+
+const _videoConstraints = VideoConstraints(
+  facingMode: FacingMode(
+    type: CameraType.user,
+    constrain: Constrain.ideal,
+  ),
+  width: VideoSize(ideal: 1920, maximum: 1920),
+  height: VideoSize(ideal: 1080, maximum: 1080),
+);
+
+class SessionPage extends StatelessWidget {
+  const SessionPage({Key? key}) : super(key: key);
+
+  static Route route() {
+    return AppPageRoute(builder: (_) => const SessionPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => SessionBloc(),
+      child: Navigator(
+        onGenerateRoute: (_) => AppPageRoute(
+          builder: (_) => const SessionView(),
+        ),
+      ),
+    );
+  }
+}
+
+class SessionView extends StatefulWidget {
+  const SessionView({Key? key}) : super(key: key);
+
+  @override
+  _SessionViewState createState() => _SessionViewState();
+}
+
+class _SessionViewState extends State<SessionView> {
+  final _controller = CameraController(
+    options: const CameraOptions(
+      audio: AudioConstraints(enabled: false),
+      video: _videoConstraints,
+    ),
+  );
+
+  bool get _isCameraAvailable =>
+      _controller.value.status == CameraStatus.available;
+
+  Future<void> _play() async {
+    if (!_isCameraAvailable) return;
+    return _controller.play();
+  }
+
+  Future<void> _stop() async {
+    if (!_isCameraAvailable) return;
+    return _controller.stop();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCameraController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeCameraController() async {
+    await _controller.initialize();
+    await _play();
+  }
+
+  void _onSnapPressed({required double aspectRatio}) async {
+    final picture = await _controller.takePicture();
+    context
+        .read<SessionBloc>()
+        .add(PhotoCaptured(aspectRatio: aspectRatio, image: picture));
+    final stickersPage = StickersPage.route();
+    await _stop();
+    unawaited(Navigator.of(context).pushReplacement(stickersPage));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final aspectRatio = orientation == Orientation.portrait
+        ? PhotoboothAspectRatio.portrait
+        : PhotoboothAspectRatio.landscape;
+    return Scaffold(
+      body: _SessionBackground(
+        aspectRatio: aspectRatio,
+        child: Camera(
+          controller: _controller,
+          placeholder: (_) => const SizedBox(),
+          preview: (context, preview) => PhotoboothPreview(
+            preview: preview,
+            onSnapPressed: () => _onSnapPressed(aspectRatio: aspectRatio),
+          ),
+          error: (context, error) => PhotoboothError(error: error),
+        ),
+      ),
+    );
+  }
+}
+
+class _SessionBackground extends StatelessWidget {
+  const _SessionBackground({
+    Key? key,
+    required this.aspectRatio,
+    required this.child,
+  }) : super(key: key);
+
+  final double aspectRatio;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const PhotoboothBackground(),
+        Center(
+          child: AspectRatio(
+            aspectRatio: aspectRatio,
+            child: Container(
+              color: PhotoboothColors.black,
+              child: child,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
